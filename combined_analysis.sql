a08aa44791bc5ef3390ccf99e45d148c4e1c5da7
Nick Papa <124495816+NikosPapa@users.noreply.github.com>
2026-01-16 12:35:27 +0100

    Add files via upload

---
+++ combined_analysis.sql
@@ -0,0 +1,50 @@
+-- Combine the two tables into a single table. 
+
+select * 
+from menu_items;
+
+select * 
+from order_details;
+
+select * 
+from order_details od left join menu_items mi
+on od.item_id = mi.menu_item_id;
+
+-- What were the least and most ordered items? What categories were they in? 
+
+select item_name, category, count(order_details_id) as num_purchases 
+from order_details od left join menu_items mi
+	on od.item_id = mi.menu_item_id
+group by item_name, category
+order by num_purchases; # Chicken Tacos is the most popular item.
+
+select item_name, category, count(order_details_id) as num_purchases 
+from order_details od left join menu_items mi
+	on od.item_id = mi.menu_item_id
+group by item_name, category
+order by num_purchases desc; # Hamburger is the least popular item.
+
+-- What were the top 5 orders that spent the most money? 
+
+select order_id, sum(price) as total_spend
+from order_details od left join menu_items mi
+	on od.item_id = mi.menu_item_id
+group by order_id
+order by total_spend desc
+limit 5;
+
+-- View the details of the highest spend order. What insights can you gather from the 
+
+select category, count(item_id) as num_items 
+from order_details od left join menu_items mi
+	on od.item_id = mi.menu_item_id
+where order_id = 440
+group by category; 
+
+-- View the details of the top 5 highest spend orders (as before). 
+
+select order_id, category, count(item_id) as num_items 
+from order_details od left join menu_items mi
+	on od.item_id = mi.menu_item_id
+where order_id in (440, 2075, 1957, 330, 2675)
+group by order_id, category; 
